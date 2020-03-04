db3ca3959d9d8fa8444dcf9cf74c9b12268d30fb
Bruno Luiz de Siqueira <brunoluizdesiqueira@gmail.com>
2020-03-04 10:48:56 -0300

    Created Notification Controller

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,6 +22,8 @@ routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/schedule', ScheduleController.index);
+routes.get('/Notifications', NotificationController.index);
+routes.put('/Notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
