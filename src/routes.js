6847ec2e3186e2c1efaa3708352bd1598c736f32
Bruno Luiz de Siqueira <brunoluizdesiqueira@gmail.com>
2020-03-04 11:32:59 -0300

    Implemented cancel the appointment

--- src/routes.js
+++ src/routes.js
@@ -21,9 +21,11 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 routes.get('/schedule', ScheduleController.index);
 routes.get('/Notifications', NotificationController.index);
 routes.put('/Notifications/:id', NotificationController.update);
+
 
 routes.post('/files', upload.single('file'), FileController.store);
 
