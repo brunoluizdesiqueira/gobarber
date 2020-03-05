a7854b776fd1387cd8d167786feb35dd1fe09b58
Bruno Luiz de Siqueira <brunoluizdesiqueira@gmail.com>
2020-03-05 12:10:52 -0300

    Implemented send Mail with canceled the appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd2a6b46ae983cd',
+    pass: 'de3352039a68ce',
+  },
+  default: {
+    from: `Equipe GoBarber <noreply@gobarber.com>`,
+  },
+};
---
+++ src/lib/Mail.js
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer';
+import mailConfig from '../config/mail';
+
+class Mail {
+  constructor() {
+    const { host, port, secure, auth } = mailConfig;
+    this.transporter = nodemailer.createTransport({
+      host,
+      port,
+      secure,
+      auth: auth.user ? auth : null,
+    });
+  }
+
+  sendMail(message) {
+    return this.transporter.sendMail(
+      {
+        ...mailConfig.default,
+        ...message,
+      },
+      error => console.log(error.message),
+      info => console.log(info.messageId)
+    );
+  }
+}
+
+export default new Mail();
